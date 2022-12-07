b00fb6d46e04cc65fead708873a4d38fadc55d7f
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-07 10:57:51 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,40 @@
+export const item = {
+    "id": "7cfc6d49-e602-44a5-9c25-e6533949c227",
+    "home_feed": [
+        {
+            "id": "b608216c-b56a-4663-abae-918daf46abe4",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png",
+            "updated_time": "here",
+            "song_name": "charge",
+            "about_song": "Right tree fire early drug theory. Response parent section type kind. Yourself develop example about fact."
+        },
+        {
+            "id": "0cad6313-95f9-48f3-ad9c-5ec6a59a0b5d",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png",
+            "updated_time": "of",
+            "song_name": "play",
+            "about_song": "He subject bad send when entire. Training include reach benefit gun air compare matter."
+        },
+        {
+            "id": "6ec8a90b-5174-42dd-b3b3-a8c9e13800d7",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png",
+            "updated_time": "table",
+            "song_name": "a",
+            "about_song": "Item forget only analysis perform. Condition adult they respond. Lead drive manage guess soldier seek add safe. Stand reflect very cold."
+        },
+        {
+            "id": "37ecd639-356a-49d5-bfb9-082f5b6626a0",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png",
+            "updated_time": "outside",
+            "song_name": "its",
+            "about_song": "So lead cell bank. Make now him. Rule right medical leave enough save threat factor."
+        },
+        {
+            "id": "d60d7d7a-382f-479d-9c90-a617de900405",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png",
+            "updated_time": "read",
+            "song_name": "dream",
+            "about_song": "Dream shoulder enjoy two. Inside move identify enter. Power reach conference this health."
+        }
+    ]
+}
